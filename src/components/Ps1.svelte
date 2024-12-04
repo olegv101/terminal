76fbf5df2cc1d92f636a4ba83e9848fdcaa15e22
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-12-03 22:23:41 -0800

    added stuff

--- src/components/Ps1.svelte
+++ src/components/Ps1.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <h1 class="font-bold flex">
-  <span style={`color: ${$theme.yellow};`}>guest</span>
+  <span style={`color: ${$theme.yellow};`}>oleg</span>
   <span style={`color: ${$theme.white}`}>@</span>
   <span style={`color: ${$theme.green}`}>{hostname}</span>
   <span style={`color: ${$theme.white}`}>:~$</span>
